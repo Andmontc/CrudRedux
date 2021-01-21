3a8f819ac8c7be01ab0b5e93a3b2f60d6f00a53b
Andmontc <oscarandresmontes@gmail.com>
2021-01-21 16:29:59 -0500

    main reducer file

---
+++ src/reducers/productosReducer.js
@@ -0,0 +1,34 @@
+import {
+	AGREGAR_PRODUCTO, AGREGAR_PRODUCTO_EXITO, AGREGAR_PRODUCTO_ERROR 
+} from'../types';
+	
+// Each reducer has their own state
+const initialState = {
+	productos: [],
+	error: false,
+	loading: false
+}
+
+export default function (state = initialState, action) {
+	switch(action.type) {
+		case AGREGAR_PRODUCTO:
+			return {
+				...state,
+				loading:action.payload
+			}
+		case AGREGAR_PRODUCTO_EXITO:
+			return {
+				...state,
+				loading:false,
+				productos: [...state.productos, action.payload]
+			}
+		case AGREGAR_PRODUCTO_ERROR:
+			return {
+				...state,
+				loading: false,
+				error: action.payload
+			}
+		default:
+			return state;
+	}
+}
